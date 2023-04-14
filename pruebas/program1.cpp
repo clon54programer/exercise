f26f27741932e6c2d0bd6fef434d1ab92b90c496
Clon5454 <clon5454@outlook.com>
2023-04-13 22:06:19 -0500

    terminar la verifcacion de la jerquia

--- pruebas/program1.cpp
+++ pruebas/program1.cpp
@@ -13,6 +13,7 @@ private:
 public:
     Personal(string n, double s, int y, char p);
     ~Personal();
+    int verificacion(char x);
 };
 
 Personal::Personal(string n, double s, int y, char p)
@@ -26,7 +27,28 @@ Personal::Personal(string n, double s, int y, char p)
 Personal::~Personal()
 {
 }
-
+int Personal::verificacion(char x)
+{
+    if (x == 'p')
+    {
+        return 1;
+    }
+    else if (x == 'j')
+    {
+        return 2;
+    }
+    else if (x == 'e')
+    {
+        return 3;
+    }
+    else
+    {
+        cout << "coloca la letra de forma incorrecta\n";
+    }
+}
+void Jerequia(int y)
+{
+}
 int main(int argc, char const *argv[])
 {
     //  variables de interacion
@@ -53,5 +75,8 @@ int main(int argc, char const *argv[])
     Personal myOBJ(name, salay, añosEnLaEmpresa, puesto);
     cout << "nombre: \t" << name << "\n salario: \t " << salay << "\naños en la empresa: \t" << añosEnLaEmpresa << "\n puesto: \t" << puesto << endl;
 
+    int Verificacion = myOBJ.verificacion(puesto);
+
+    Jerequia(Verificacion);
     return 0;
 }
