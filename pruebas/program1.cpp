0b778fbe6ddd92ac5486ef665e7699c785f7acdc
Clon5454 <clon5454@outlook.com>
2023-04-13 21:36:09 -0500

    creando la base del programa

--- pruebas/program1.cpp
+++ pruebas/program1.cpp
@@ -5,13 +5,17 @@ using namespace std;
 class Personal
 {
 private:
-    /* data */
+    string name;
+    double salary;
+    int añosEnLaEmpresa;
+    char puesto;
+
 public:
-    Personal(/* args */);
+    Personal(string n, double s, int y, char p);
     ~Personal();
 };
 
-Personal::Personal(/* args */)
+Personal::Personal(string n, double s, int y, char p)
 {
 }
 
@@ -21,12 +25,22 @@ Personal::~Personal()
 
 int main(int argc, char const *argv[])
 {
-    cout << "ingrese su nombre\n";
+    // variables de interacion
     string name;
-    cin >> name;
-    cout << "ingrese su salario";
     double salay;
+    int añosEnLaEmpresa;
+    char puesto;
+
+    cout << "ingrese su nombre\n";
+    cin >> name;
+
+    cout << "ingrese su salario\n";
     cin >> salay;
+
+    cout << "ingrese cuantos años a trabajado en le empresa\n";
+    cin >> añosEnLaEmpresa;
+
+    cout << "escriba p si tiene entre 0 hasta 2 años en la empresa,\n escriba j si tiene 2 entre y 5 años en la empresa,\n escroba e si tine mas de 5 años en la empresa  \n";
 
     return 0;
 }
