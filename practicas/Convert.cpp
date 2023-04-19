390bf3866176c4ce4af97711c264d9422fcd606f
Clon5454 <clon5454@outlook.com>
2023-04-19 18:57:26 -0500

    practica de conversion

---
+++ practicas/Convert.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // varibles
+    string age = "12";
+    cout >> "conversion de integer a string\n";
+    if (typeof(age) == "12")
+    {
+        cout << "es un string";
+    }
+
+    string juan = 12;
+
+    return 0;
+}
