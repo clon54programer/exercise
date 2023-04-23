b0fecc09297c0789b6d14992a783fb67b96c8e54
Clon5454 <clon5454@outlook.com>
2023-04-22 20:57:48 -0500

    prueba de continue

---
+++ pruebas/prover-continue.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+/*en este archivo voy a practicar el uso de continue*/
+
+int main(int argc, char const *argv[])
+{
+    cout << "ingrese su contraseña";
+    return 0;
+}
