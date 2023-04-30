e77e70dad657ddab15d7746de0f4f798d430b2f7
Clon5454 <clon5454@outlook.com>
2023-04-29 23:40:28 -0500

    he creado la clase principal y las varibles de interacion

--- pruebas/info/login con POO/main.cpp
+++ pruebas/info/login con POO/main.cpp
@@ -2,9 +2,34 @@
 
 using namespace std;
 
+class RegitryUsuary
+{
+private:
+    /* data */
+public:
+    RegitryUsuary(/* args */);
+    ~RegitryUsuary();
+};
+
+RegitryUsuary::RegitryUsuary(/* args */)
+{
+}
+
+RegitryUsuary::~RegitryUsuary()
+{
+}
+
 int main(int argc, char const *argv[])
 {
+    // crearemos un login
+    setlocale(LC_ALL, "es_ES.UTF-8");
+    // varibles
+    string nombre;
+    string apellido;
+    int ID;
+    int numeroDeTelefono;
+    string correo;
 
-    cout << "hello world";
+    cout << "buenos dias señor usuario\n por favor deme unos datos para poder registrarse";
     return 0;
 }
