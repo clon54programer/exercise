197ef28a32b9c5c2499368b3a86cd16286ff687b
Clon5454 <clon5454@outlook.com>
2023-05-04 21:57:29 -0500

    docuemntando codigo

--- POO/header/mate.h
+++ POO/header/mate.h
@@ -1,4 +1,8 @@
 #pragma once
+/*para usar la programacion orintada a objetos se debe primereo declarar una cabezara
+despues debemos crear un archivo con la extension.cpp con el mismo nombre del heder*/
+// el header creamos los metodos y variables pero no las definimos.
+// los metodos se deben definir en el archivo con la extension .cpp.
 class carro
 {
 public:
--- POO/main.cpp
+++ POO/main.cpp
@@ -11,13 +11,14 @@ int main(int argc, char const *argv[])
     // variables
     double x = 0.0;
     double y = 0.0;
-    double result = myOBJ.suma(x, y);
+    double result = 0.0;
 
     cout << "deme un numero\n";
     cin >> x;
     cout << "deme otro numero\n";
     cin >> y;
 
+    result = myOBJ.suma(x, y);
     cout << "el resultado es " << result;
     return 0;
 }
