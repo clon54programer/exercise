37209b0199e3ef57c738a1f63ee01cc9df08a092
Clon5454 <clon5454@outlook.com>
2023-07-07 06:48:29 -0500

    base

--- pruebas/ejercios/ruleta rusa/main.cpp
+++ pruebas/ejercios/ruleta rusa/main.cpp
@@ -3,8 +3,17 @@
 
 using namespace std;
 
+double RuletaRusa()
+{
+}
+
 int main(int argc, char const *argv[])
 {
+    // variables
+    int InputNimber;
+
+    cout << "deme un numero" << endl;
+    cin >> InputNimber;
 
     return 0;
 }
