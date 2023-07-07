a0bd9d749ba96a2bd2f9baa3758a0a1e4dc5009f
Clon5454 <clon5454@outlook.com>
2023-07-07 06:52:25 -0500

    estructura del programa casi listo

--- pruebas/ejercios/ruleta rusa/main.cpp
+++ pruebas/ejercios/ruleta rusa/main.cpp
@@ -3,16 +3,22 @@
 
 using namespace std;
 
-double RuletaRusa()
+double RuletaRusa(double x, int balas)
 {
+    x = rand() % balas;
+    return x;
 }
 
 int main(int argc, char const *argv[])
 {
     // variables
     int InputNimber;
+    int numeroDeBalas;
 
-    cout << "deme un numero" << endl;
+    cout << "deme el numerro de balas" << endl;
+    cin >> numeroDeBalas;
+
+    cout << "selecione un numero que este intervalo de " << numeroDeBalas << endl;
     cin >> InputNimber;
 
     return 0;
